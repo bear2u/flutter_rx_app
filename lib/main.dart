20231916fb74cb5d630c6ceb2ecf10c98a062fb6
김태현 <bear2u@naver.com>
2019-08-12 20:36:02 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+
+  String title;
+
+  MyHomePage({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              TextFormField(),
+              TextFormField(),
+              RaisedButton(
+                child: Text('로그인'),
+                onPressed: () {
+
+                },
+              )
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
